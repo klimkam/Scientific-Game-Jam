a0cfa0550833e5945af74421c79d09729e570110
Klimentiy Mandra <klimentiy.mandra@gmail.com>
2024-04-06 12:36:52 -0400

    Bootstrap Scene Completed
    If you want to add some sort of 3-rd party libraries that will affect the whole game
    Please add them to "WwiseScene"

--- Assets/Scripts/Bootstrap/Bootstrap.cs
+++ Assets/Scripts/Bootstrap/Bootstrap.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bootstrap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Cursor.visible = false;
+        SceneManager.LoadScene("Main Menu", LoadSceneMode.Additive);
     }
 }
