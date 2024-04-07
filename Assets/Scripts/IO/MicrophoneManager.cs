dcf61def8751933da8907ca7666223ecbd311c38
Michael Hemingway <mpghemingway@gmail.com>
2024-04-07 14:01:13 -0400

    Attempt mic detection

--- Assets/Scripts/IO/MicrophoneManager.cs
+++ Assets/Scripts/IO/MicrophoneManager.cs
@@ -1,55 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine;    
+using System.Collections;    
 
-// measures "breathing"
-public class MicrophoneManager : MonoBehaviour
-{
-    AudioClip microphoneInput;
-    bool microphoneInitialized;
-    public float sensitivity;
-    public bool flapped;
-
-    void Awake()
-    {
-        //init microphone input
-        if (Microphone.devices.Length > 0)
-        {
-            microphoneInput = Microphone.Start(Microphone.devices[0], true, 999, 44100);
-            microphoneInitialized = true;
-        }
-    }
-
-    // void Update()
-    // {
-    //     //get mic volume
-    //     int dec = 128;
-    //     float[] waveData = new float[dec];
-    //     int micPosition = Microphone.GetPosition(null) - (dec + 1); // null means the first microphone
-    //     microphoneInput.GetData(waveData, micPosition);
-
-    //     // Getting a peak on the last 128 samples
-    //     float levelMax = 0;
-    //     for (int i = 0; i < dec; i++)
-    //     {
-    //         float wavePeak = waveData[i] * waveData[i];
-    //         if (levelMax < wavePeak)
-    //         {
-    //             levelMax = wavePeak;
-    //         }
-    //     }
-    //     float level = Mathf.Sqrt(Mathf.Sqrt(levelMax));
-
-    //     if (level > sensitivity && !flapped)
-    //     {
-    //         Flap();
-    //         flapped = true;
-    //     }
-    //     if (level < sensitivity && flapped)
-    //         flapped = false;
-    // }
-
-    // void Flap () {
-
-    // }
+// Measures "breathing"
+[RequireComponent (typeof (AudioSource))]    
+public class MicrophoneManager : MonoBehaviour     
+{    
+    // Boolean flags shows if the microphone is connected   
+    private bool micConnected = false;    
+    
+    //The maximum and minimum available recording frequencies    
+    private int minFreq;    
+    private int maxFreq;    
+    
+    //A handle to the attached AudioSource    
+    private AudioSource goAudioSource;    
+     
+    void Start()     
+    {    
+        //Check if there is at least one microphone connected    
+        if(Microphone.devices.Length <= 0)    
+        {    
+            //Throw a warning message at the console if there isn't    
+            Debug.LogWarning("Microphone not connected!");    
+        }    
+        else //At least one microphone is present    
+        {    
+            //Set our flag 'micConnected' to true    
+            micConnected = true;    
+    
+            //Get the default microphone recording capabilities    
+            Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);    
+    
+            //According to the documentation, if minFreq and maxFreq are zero, the microphone supports any frequency...    
+            if(minFreq == 0 && maxFreq == 0)    
+            {    
+                //...meaning 44100 Hz can be used as the recording sampling rate    
+                maxFreq = 44100;    
+            }    
+    
+            //Get the attached AudioSource component    
+            goAudioSource = this.GetComponent<AudioSource>();    
+        }    
+    }    
+    
+    void OnGUI()     
+    {    
+        //If there is a microphone    
+        if(micConnected)    
+        {    
+            //If the audio from any microphone isn't being captured    
+            if(!Microphone.IsRecording(null))    
+            {    
+                //Case the 'Record' button gets pressed    
+                if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Record"))    
+                {    
+                    //Start recording and store the audio captured from the microphone at the AudioClip in the AudioSource    
+                    goAudioSource.clip = Microphone.Start(null, true, 20, maxFreq);    
+                }    
+            }    
+            else //Recording is in progress    
+            {    
+                //Case the 'Stop and Play' button gets pressed    
+                if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Stop and Play!"))    
+                {    
+                    Microphone.End(null); //Stop the audio recording    
+                    goAudioSource.Play(); //Playback the recorded audio    
+                }    
+    
+                GUI.Label(new Rect(Screen.width/2-100, Screen.height/2+25, 200, 50), "Recording in progress...");    
+            }    
+        }    
+        else // No microphone    
+        {    
+            //Print a red "Microphone not connected!" message at the center of the screen    
+            GUI.contentColor = Color.red;    
+            GUI.Label(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Microphone not connected!");    
+        }    
+    
+    }    
 }
